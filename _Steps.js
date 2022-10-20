f6ec73aeda501e4659951ffce109f99328da936f
aknathweb <aniknwu.cse@gmail.com>
2022-10-20 21:23:16 +0600

    setup to perform server site task using nodejs expressjs framework

---
+++ _Steps.js
@@ -0,0 +1,10 @@
+/* 
+Express.js, or simply Express, is a back end web application framework for building RESTful APIs with Node.js
+
+Installing:
+>npm init (package.json)
+>>create index.js
+>>>npm i express (for perform backend task)
+>>>>npm i nodemon (for node monitor and easy to refresh base on change)
+>>>>>create .gitignore > node_modules (to skip send node_modules file in github)
+*/
