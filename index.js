f5e0aa028c312b56b39d62c4ff3a5a8b5a8ff80b
aknathweb <aniknwu.cse@gmail.com>
2022-10-21 22:51:07 +0600

    different path wise data handle and send

--- index.js
+++ index.js
@@ -14,9 +14,30 @@ or, open for everyone excess
 const cors = require('cors');
 app.use(cors());
 
+//here,me make my own api start
 app.get('/', (req, res) => {
     res.send('my own api');
 })
+const categories = require('./JSON_Data/Categories.json');
+app.get('/categories', (req, res) => {
+    res.send(categories);
+    // console.log(categories);
+})
+
+const allNews = require('./JSON_Data/News.json');
+
+app.get('/news', (req, res) => {
+    res.send(allNews)
+})
+app.get('/news/:id', (req, res) => {
+    const news = allNews.find(news => news._id === req.params.id);
+    res.send(news);
+})
+app.get('/category/:id', (req, res) => {
+    const category_news = allNews.filter(category_news => category_news.category_id === req.params.id);
+    res.send(category_news);
+})
+//here,me make my own api end
 
 
 // to check the port are running or not start
