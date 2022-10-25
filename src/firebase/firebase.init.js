0216f23c697ca60d0815c5e0e30cb8e8277633c6
Anik Kumar Nath (Web) <108430535+aknathweb@users.noreply.github.com>
2022-10-25 23:49:26 +0600

    secure firebase config file using environment variable

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -5,12 +5,19 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyA-YIBZarpuLVqp1n8YCfhDqU9-OG_K-hU",
-    authDomain: "news-portal-4b93e.firebaseapp.com",
-    projectId: "news-portal-4b93e",
-    storageBucket: "news-portal-4b93e.appspot.com",
-    messagingSenderId: "892945107471",
-    appId: "1:892945107471:web:1c81971ad8f0ae4a57317a"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
+    
+//     apiKey: "AIzaSyA-YIBZarpuLVqp1n8YCfhDqU9-OG_K-hU",
+//     authDomain: "news-portal-4b93e.firebaseapp.com",
+//     projectId: "news-portal-4b93e",
+//     storageBucket: "news-portal-4b93e.appspot.com",
+//     messagingSenderId: "892945107471",
+//     appId: "1:892945107471:web:1c81971ad8f0ae4a57317a"
 };
 
 // Initialize Firebase
